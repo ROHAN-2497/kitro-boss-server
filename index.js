a6c99ff4e1e71f8d551c9559d95dd641cd8557e1
rohan future devoloper <xrohaislam373@gmail.com>
2023-09-15 02:36:57 +0600

    menu Item And reviews item read

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 // middle ware
-app.get('/', (req, res) => {
-  res.send('kitro boss is running ')
-})
+app.use(cors());
+app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("kitro boss is running ");
+});
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.xw5a4us.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xw5a4us.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,24 +19,39 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("kitroDb").collection("menu");
+    const reviewsCollection = client.db("kitroDb").collection("menu")
+
+    app.get('/menu', async (req, res) =>{
+      const result = await menuCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get('/reviews', async (req, res) =>{
+      const result = await reviewsCollection.find().toArray();
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-  console.log(`kitro boss is running on a port ${port}`)
-})
+  console.log(`kitro boss is running on a port ${port}`);
+});
