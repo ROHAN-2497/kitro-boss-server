0dffc00b4b0aab54bc604e2d45f8b3b19a98fcba
rohan future devoloper <xrohaislam373@gmail.com>
2023-09-21 08:14:37 +0600

    carts get operation

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ async function run() {
     })
 
     // CartSCollection
+    app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/carts', async(req, res) =>{
       const item = req.body;
