e05dccb91dd72bb81a7d1a6083a48148ba8051dd
rohan future devoloper <xrohaislam373@gmail.com>
2023-09-26 05:13:45 +0600

    user get for client side

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("kitro boss is running ");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xw5a4us.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,21 +27,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("kitroDb").collection("users");
     const menuCollection = client.db("kitroDb").collection("menu");
     const reviewsCollection = client.db("kitroDb").collection("reviews");
     const cartCollection = client.db("kitroDb").collection("carts");
+// users apis
 
+app.get('/users', async (req, res) =>{
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+})
+
+app.post('/users', async(req, res) =>{
+  const user = req.body;
+  const query = {email : user.email}
+  const existingUser = await usersCollection.findOne(query)
+  if(existingUser){
+    return res.send({message: 'user Already existing'})
+  }
+  const result = await usersCollection.insertOne(user)
+  res.send(result);
+})
+
+// menu apis
     app.get('/menu', async (req, res) =>{
       const result = await menuCollection.find().toArray();
       res.send(result)
     });
-
+// review Apis
     app.get('/reviews', async (req, res) =>{
       const result = await reviewsCollection.find().toArray();
       res.send(result)
     })
 
-    // CartSCollection
+    // CartSCollection Carts Apis
     app.get('/carts', async(req, res) =>{
       const email = req.query.email;
       if(!email){
@@ -54,9 +73,14 @@ async function run() {
 
     app.post('/carts', async(req, res) =>{
       const item = req.body;
-      console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result); 
+    })
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
